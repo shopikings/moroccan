0133b1e1409fc1d0f125ab849b8e1bf5891c4dff
Tehmeena Zafar <tehmeenazafar814@gmail.com>
2025-11-05 21:56:27 +0500

    update

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -109,7 +109,7 @@ const Shop = () => {
 
           <div className="">
             <div className="py-12">
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
                 {currentProducts.map((product) => (
                   <ProductCard key={product.id} {...product} />
                 ))}
